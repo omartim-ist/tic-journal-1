dcf9f2357c4c7eacfc701734ffff7866fa896961
Martim <martim.garcia.castro@tecnico.ulisboa.pt>
2025-12-09 16:03:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ import numpy as np
 import yfinance as yf
 
 ### 2) Import dependencies
-from config import SECURITIES, CONSTRAINTS, DATE_INIT
-from 
+from config import SECURITIES, CONSTRAINTS, DATE_INIT, N_BOOT, L, T
+from bootstrap.politis_romano import PolitisRomanoBootstrap
 
+### 3) Download data
+SECURITIES = sorted(SECURITIES)
+data = yf.download(SECURITIES, start=DATE_INIT, auto_adjust=adj_close)['Close'] 
+
+### 4) Data Generation via Politis-Romano Bootstrap
+prb = PolitisRomanoBootstrap(serie=serie, n_boot=N_BOOT, l=L, T=T)
+prb_data = prb.generate()
+
+### 5) Otimization
+
+
+### 6) Validation
