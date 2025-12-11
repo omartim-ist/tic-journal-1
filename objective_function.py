358ad3c079c161311dbd57205e968cb6031fa035
Martim <martim.garcia.castro@tecnico.ulisboa.pt>
2025-12-11 11:20:05 +0000

    Update objective_function.py

--- objective_function.py
+++ objective_function.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def variance(w, Sigma):
     return w.T @ Sigma @ w
   
@@ -9,7 +8,13 @@ def sharpe(w, mu, Sigma, rf):
     vol = np.sqrt(variance(w, Sigma))
     return (ret - r_f) / vol
 
-def get_moments(data):
+def get_moments(set):
+    d_set = np.diff(np.log(set), axis=1) # log-returns
+    mu = np.mean(d_set, axis=1)
+    Sigma = np.cov(d_set)
+    return mu, Sigma
+        
+    
     
 
 
