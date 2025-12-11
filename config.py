f2603c09afcc6003ccaeb182f2f2b036700b1669
Martim <martim.garcia.castro@tecnico.ulisboa.pt>
2025-12-11 13:23:56 +0000

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 SECURITIES = ['XLE', 'XLF', 'XLRE', 'XLI', 'XLB', 'XLK', 'XLU', 'XLC', 'XLY', 'XLP', 'XLV']
 
 # Dates
-DATE_INIT = '2018-01-01'
+DATE_INIT = '2018-07-01'
 from datetime import date, timedelta
-DATE_END = (date.today - timedelta(days=1)).strftime("%Y-%m-%d")
+DATE_END = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
 
 # Bootstrap
 N_BOOT = 1_000 # nº of series to generate
@@ -17,3 +17,4 @@ METRIC = 'variance' # variance or sharpe
 import numpy as np
 RF =  0.02 # offset for Sharpe Ratio calculation; 0.02 equals 2% annualized return
 RF = np.log((1 + RF)**(1/252))
+
