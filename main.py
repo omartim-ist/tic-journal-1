1a25ece8920a8815e081b1dc11583fe9ac06208d
Martim <martim.garcia.castro@tecnico.ulisboa.pt>
2025-12-11 11:03:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def _optimize(method='SLSQP'):
     res = minimize(
       obj_, # objetive function
       w0, # initialization
-      args=(), # extra arguments for objective function
+      args=(prb_data), # extra arguments for objective function
       method=method, # optimization method
       bounds= [(0, 1)] * n_sec # no short positions avoided
       constraints= {'type': 'eq', 'fun': lambda w: np.sum(w) - 1.0}, # sum of weights equals 1.0 (no leverage) 
