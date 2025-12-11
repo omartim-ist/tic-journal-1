62c85b34e12624df67a88827b8789c8b58dd10e7
Martim <martim.garcia.castro@tecnico.ulisboa.pt>
2025-12-11 12:33:40 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 # Optimization Universe
-SECURITIES = []
+SECURITIES = ['XLE', 'XLF', 'XLRE', 'XLI', 'XLB', 'XLK', 'XLU', 'XLC', 'XLY', 'XLP', 'XLV']
 
 # Dates
-DATE_INIT =
-DATE_END =
+DATE_INIT = '2018-01-01'
+from datetime import date, timedelta
+DATE_END = (date.today - timedelta(days=1)).strftime("%Y-%m-%d")
 
 # Bootstrap
-N_BOOT = # nº of series to generate
-L = # average length of blocks
-T = # length of each serie
+N_BOOT = 1_000 # nº of series to generate
+L = 252/12 * 4 # average length of blocks
+T = 252*5 # length of each serie
 
 # Optimization
 OPT_METHOD = 'SLSQP' # SLSQP
 METRIC = 'variance' # variance or sharpe
-
 import numpy as np
 RF =  0.02 # offset for Sharpe Ratio calculation; 0.02 equals 2% annualized return
 RF = np.log((1 + RF)**(1/252))
