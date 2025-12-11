448717f692e96ffa5a65e269975f061b2aedbaf7
Martim <martim.garcia.castro@tecnico.ulisboa.pt>
2025-12-11 11:30:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 ### 1) Import packages
 import numpy as np
 import yfinance as yf
+from joblib import Parallel, delayed
 from scipy.optimize import minimize
 
 
 ### 2) Import dependencies
 from config import SECURITIES, DATE_INIT, N_BOOT, L, T, OBJ_, OPT_METHOD
 from politis_romano import PolitisRomanoBootstrap
+from functions import get_moments
 from optimizer import Optimizer
 
 
@@ -23,12 +25,16 @@ prb_data = prb.generate()
 
 
 ### 5) Optimization
+moments_l = Parallel(n_jobs=-1)(
+    delayed(get_moments)(set_) for set_ in prb_data
+)
+
 obj_ = OBJ_
-def _optimize(method='SLSQP'):
+def _optimize():
     res = minimize(
       obj_, # objetive function
       w0, # initialization
-      args=(prb_data), # extra arguments for objective function
+      args=(moments_l), # extra arguments for objective function
       method=method, # optimization method
       bounds= [(0, 1)] * n_sec # no short positions avoided
       constraints= {'type': 'eq', 'fun': lambda w: np.sum(w) - 1.0}, # sum of weights equals 1.0 (no leverage) 
@@ -36,7 +42,7 @@ def _optimize(method='SLSQP'):
     return res.x
 
 w0 = np.ones(n_sec) / n_sec  # Initialization (equal weighted portfolio)
-w_opt = _optimize(OPT_METHOD)
+w_opt = _optimize()
 
 
 ### 6) Validation
