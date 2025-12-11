0a48ab53293368106c050199dc781fe11b48b652
Martim <martim.garcia.castro@tecnico.ulisboa.pt>
2025-12-11 12:19:31 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
-# SECURITIES
+# Optimization Universe
 SECURITIES = []
 
 # Dates
 DATE_INIT =
+DATE_END =
 
 # Bootstrap
 N_BOOT = # nº of series to generate
@@ -10,4 +11,9 @@ L = # average length of blocks
 T = # length of each serie
 
 # Optimization
-OPT_METHOD = 'SLSQP' # SLSQP, 
+OPT_METHOD = 'SLSQP' # SLSQP
+METRIC = 'variance' # variance or sharpe
+
+import numpy as np
+RF =  0.02 # offset for Sharpe Ratio calculation; 0.02 equals 2% annualized return
+RF = np.log((1 + RF)**(1/252))
