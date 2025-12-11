3ae47ae59e977b30bba1639ad76bd59849cff129
Martim <martim.garcia.castro@tecnico.ulisboa.pt>
2025-12-11 13:24:58 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import yfinance as yf
 from joblib import Parallel, delayed
 from scipy.optimize import minimize
+import time # for debug
 
 
 ### 2) Import dependencies
@@ -13,7 +14,7 @@ from functions import get_moments, _OBJ
 
 ### 3) Download data
 SECURITIES = sorted(SECURITIES)
-data = yf.download(SECURITIES, start=DATE_INIT, end=DATE_END, auto_adjust=adj_close)['Close'] 
+data = yf.download(SECURITIES, start=DATE_INIT, end=DATE_END, auto_adjust=True)['Close'] 
 data_np = data.to_numpy().T
 n_sec = len(SECURITIES)
 
@@ -35,13 +36,16 @@ def _optimize():
       w0, # initialization
       args=(moments_l, METRIC, RF,), # extra arguments for objective function
       method=OPT_METHOD, # optimization method
-      bounds= [(0, 1)] * n_sec # no short positions avoided
+      bounds= [(0, 1)] * n_sec, # no short positions avoided
       constraints= {'type': 'eq', 'fun': lambda w: np.sum(w) - 1.0}, # sum of weights equals 1.0 (no leverage) 
     )
     return res.x
 
 w0 = np.ones(n_sec) / n_sec  # Initialization (equal weighted portfolio)
+ti = time.time()
 w_opt = _optimize()
+print(time.time() - ti)
 
 
 ### 6) Validation
+
