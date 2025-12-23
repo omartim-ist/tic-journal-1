7a112b19b229c29e15485160ef4a2d62267a33bc
Martim <martim.garcia.castro@tecnico.ulisboa.pt>
2025-12-23 22:16:16 +0000

    Update main.py

--- ClusteringPO/main.py
+++ ClusteringPO/main.py
@@ -1,13 +1,10 @@
 ### 1) Import packages
 import numpy as np
 import yfinance as yf
-from joblib import Parallel, delayed
-from scipy.optimize import minimize
-import time # for debug
 
 
 ### 2) Import dependencies
-from config import SECURITIES, DATE_INIT, DATE_END, N_BOOT, L, T, RF
+from config import SECURITIES, DATE_INIT, DATE_END, N_BOOT, L, T
 from hierarchical_clustering import HierarchicalClustering
 from politis_romano import PolitisRomanoBootstrap
 
@@ -34,34 +31,6 @@ while True:
 
 
 ### 5) Intra-Cluster Portfolios
-def get_ICP(assets_index, Mu):
-    
-    if len(assets_index) == 1:
-        return np.array([1.0])
-        
-    Sigma = np.sqrt([Var[index, index] for index in assets_index])
-    Mu = [Mu[index] for index in assets_index]
-    
-    if len(assets_index) == 2:
-        w = np.maximum(Mu, 0.0) / Sigma
-        w /= w.sum()
-        return w
-        
-    Mu = np.abs(Mu)
-    # Linear regression
-    X = np.column_stack([np.ones(len(Mu)), Mu])
-    beta = np.linalg.lstsq(X, Sigma, rcond=None)[0]
-    a, b = beta
-
-    Sigma_adj = Sigma - b * Mu
-
-    # pesos intra-cluster
-    w = 1.0 / Sigma_adj
-    w /= w.sum()
-    
-    return w
-
-
 Var = np.cov(lrets)
 Mu = np.mean(lrets, axis=1)
 asset_weights = {}
@@ -72,4 +41,4 @@ for cluster, assets_index in clusters.items():
         asset_weights[idx] = (cluster, w)
 
 
-
+### 6) Inter-Clustering Portfolio
