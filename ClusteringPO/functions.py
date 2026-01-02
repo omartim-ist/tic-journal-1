1ec2c7de04151448e898c97273cc673f76b49782
Martim <martim.garcia.castro@tecnico.ulisboa.pt>
2026-01-02 23:54:49 +0000

    Update functions.py
    risk parity em functions.py

--- ClusteringPO/functions.py
+++ ClusteringPO/functions.py
@@ -1,6 +1,10 @@
 import numpy as np
+from scipy.optimize import minimize
 
-# Intra-Clustering Portfolio
+
+
+''' Intra-Clustering Portfolio '''
+
 def get_ICP(assets_index, Mu):
     """
     Computes intra-cluster portfolio weights adjusted for the return–volatility relationship.
@@ -53,4 +57,23 @@ def get_ICP(assets_index, Mu):
     w /= w.sum()
     
     return w
+
+
+
+''' Risk Parity '''
+
+def RC(x, Sigma):
+    sigma = np.sqrt(x.T @ Sigma @ x)
+    return x*(Sigma @ x)/sigma
+
+def objective(x,Sigma):
+    Rc = RC(x,Sigma)
+    return sum([(Rc[i] - Rc[j])**2 for i in range(len(Rc)) for j in range(len(Rc))])
+
+def RiskParity(Sigma):    
+    x_0 = np.array([1/len(Sigma) for i in range(len(Sigma))])
+    cons = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+    bounds = [(0, None) for _ in range(len(Sigma))]
+    res = minimize(objective, x_0, args=(Sigma,), constraints=cons, method='SLSQP', bounds=bounds)
+    return res
 
