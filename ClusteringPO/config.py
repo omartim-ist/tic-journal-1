d9fd0b9f5b30f1fd26e7ac1e5bf3a2531d847f71
Martim <martim.garcia.castro@tecnico.ulisboa.pt>
2026-01-02 23:48:56 +0000

    Update config.py

--- ClusteringPO/config.py
+++ ClusteringPO/config.py
@@ -10,10 +10,3 @@ DATE_END = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
 N_BOOT = 1_000 # nº of series to generate
 L = 252/12 * 4 # average length of blocks
 T = 252*5 # length of each serie
-
-# Optimization
-OPT_METHOD = 'SLSQP' # SLSQP
-METRIC = 'variance' # variance or sharpe
-import numpy as np
-RF =  0.02 # offset for Sharpe Ratio calculation; 0.02 equals 2% annualized return
-RF = np.log((1 + RF)**(1/252))
