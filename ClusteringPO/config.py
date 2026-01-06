8b9e0b4abed2c3f8381fe3d698e6f3f273caa547
Martim <martim.garcia.castro@tecnico.ulisboa.pt>
2026-01-06 12:09:44 +0000

    Update config.py

--- ClusteringPO/config.py
+++ ClusteringPO/config.py
@@ -1,12 +1,13 @@
 # Optimization Universe
-SECURITIES = ['XLE', 'XLF', 'XLRE', 'XLI', 'XLB', 'XLK', 'XLU', 'XLC', 'XLY', 'XLP', 'XLV']
+SECURITIES = ['XLE', 'XLF', 'XLRE', 'XLI', 'XLB', 'XLK', 'XLU', 'XLC', 'XLY', 'XLP', 'XLV', 'BTC-USD']
 
 # Dates
-DATE_INIT = '2018-07-01'
+DATE_INIT = '2018-07-02'
 from datetime import date, timedelta
 DATE_END = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
 
 # Bootstrap
-N_BOOT = 1_000 # nº of series to generate
-L = 252/12 * 4 # average length of blocks
-T = 252*5 # length of each serie
+N_BOOT = 10_000 # nº of series to generate
+L = 365/12 * 4 # average length of blocks
+T = 365*5 # length of each serie
+
