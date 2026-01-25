fc80529f5f2eeb758e9a6946cc6c9de0544766c0
afonsombarao <afonso.j.m.barao@gmail.com>
2026-01-25 21:42:46 +0000

    Implementação do PCA
    Pus aqui o ficheiro com a implementação do pca. os resultados sao relativos aos walk forwards com blocos de 44 data points, onde n_train_blocks = 2 e n_test_blocks = 1. depois de decompor a matriz da covariância, para alocar os pesos, apliquei uma mean-reversion.

---
+++ pcanalysis.py
@@ -0,0 +1,60 @@
+def pca_mean_reversion(lrets):
+    Cov = np.cov(lrets)
+    eigenvalues, eigenvectors = np.linalg.eigh(Cov)
+    w_pc1 = eigenvectors[:,-1]
+    w_pc1 = w_pc1/w_pc1.sum()
+    w_pc2 = eigenvectors[:,-2]
+    w_pc2 = w_pc2/w_pc2.sum()
+    mu_pc1 = np.mean(w_pc1 @ lrets)
+    mu_pc2 = np.mean(w_pc2 @ lrets)
+    if mu_pc1 > mu_pc2:
+        return w_pc2
+    else:
+        return w_pc1
+    
+def test_train_data(data, n_splits, n_train_blocks, n_test_blocks):
+    len_blocks = len(data[0])//n_splits
+    train_data = []
+    test_data = []
+
+    limit_loop = n_splits - max(n_train_blocks, n_test_blocks) + 1
+    
+    for k in range(limit_loop):
+        train_data += [data[:,k*len_blocks:(k+n_train_blocks)*len_blocks]]
+        test_data += [data[:,k*len_blocks:(k+n_test_blocks)*len_blocks]]
+    return np.array(test_data), np.array(train_data)
+
+test_data, train_data = test_train_data(lrets, 44, 2, 1)
+
+w = []
+
+for i in range(len(train_data)):
+    w += [pca_mean_reversion(train_data[i])]
+
+w = np.array(w)
+
+test = []
+for i in range(len(train_data)-2):
+    test += [w[i] @ test_data[i+2]]
+
+test = np.array(test)
+
+results = np.concatenate(test)
+
+plt.figure(figsize=(12, 6))
+plt.plot(results, linewidth=1)
+plt.xlabel('Date')
+plt.ylabel('Return ($)')
+plt.grid(True, alpha=0.3)
+plt.tight_layout()
+plt.show()
+
+portfolio = np.exp(np.cumsum(results))
+
+plt.figure(figsize=(12, 6))
+plt.plot(portfolio, linewidth=1)
+plt.xlabel('Date')
+plt.ylabel('Portfolio Value ($)')
+plt.grid(True, alpha=0.3)
+plt.tight_layout()
+plt.show()
